d25ba859e7b32342adbe5186c636a9a02d6c0069
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-09-11 21:50:39 +0900

    Named route

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/screens/home_screen.dart';
+import 'package:navigation/screens/route_one_screen.dart';
+import 'package:navigation/screens/route_three_screen.dart';
+import 'package:navigation/screens/route_two_screen.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: HomeScreen(),
-      )
-  );
+  runApp(MaterialApp(
+    // home: HomeScreen(),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomeScreen(),
+      '/one': (context) => RouteOneScreen(),
+      '/two': (context) => RouteTwoScreen(),
+      '/three': (context) => RouteThreeScreen(),
+    },
+  ));
 }
--- lib/screens/route_one_screen.dart
+++ lib/screens/route_one_screen.dart
@@ -3,9 +3,9 @@ import 'package:navigation/layouts/main_layout.dart';
 import 'package:navigation/screens/route_two_screen.dart';
 
 class RouteOneScreen extends StatelessWidget {
-  final int number;
+  final int? number;
   const RouteOneScreen({
-    required this.number,
+    this.number,
     Key? key,
   }) : super(key: key);
 
